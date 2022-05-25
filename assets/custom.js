5866d97d4aa0d2d8aff10d66becc34cc62a8e27e
= <andylackk@outlook.com>
2022-05-25 12:34:42 -0500

    first commit

---
+++ assets/custom.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function() {
+    $('.smooth-scroll').on('click', function(evt) {
+      evt.preventDefault();
+      $('html, body').animate(
+        { scrollTop: $($(this).attr('href')).offset().top},
+        300
+      );
+    });
+  });
